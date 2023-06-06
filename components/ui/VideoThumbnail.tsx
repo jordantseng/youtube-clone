88d38511d7eaba5682c63e968f851a73acb392f9
Jordan <jordantseng1024@gmail.com>
2023-06-06 14:12:13 +0800

    add onClick pro to video thumbnail component

--- components/ui/VideoThumbnail.tsx
+++ components/ui/VideoThumbnail.tsx
@@ -5,12 +5,17 @@ import { transformDuration } from '@/lib/util';
 type VideoThumbnailProps = {
   thumbnail: string;
   duration: string;
+  onClick: () => void
 };
 
 // TODO: Image width and height
-const VideoThumbnail = ({ thumbnail, duration }: VideoThumbnailProps) => {
+const VideoThumbnail = ({
+  thumbnail,
+  duration,
+  onClick,
+}: VideoThumbnailProps) => {
   return (
-    <div className="relative">
+    <div className="relative cursor-pointer" onClick={() => onClick()}>
       <Image
         className="w-full rounded-2xl"
         src={thumbnail}
@@ -18,7 +23,7 @@ const VideoThumbnail = ({ thumbnail, duration }: VideoThumbnailProps) => {
         height={180}
         alt=""
       />
-      <div className="absolute bottom-1 right-2 p-1 bg-zinc-900/90 text-white text-xs font-medium rounded-md">
+      <div className="absolute bottom-2 right-2 p-1 bg-zinc-900/90 text-white text-xs font-medium rounded-md">
         {transformDuration(duration)}
       </div>
     </div>
