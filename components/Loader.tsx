9840d64808d34e22e345e1ea0b58ea67ab008a95
Jordan <jordantseng1024@gmail.com>
2023-06-11 12:43:21 +0800

    change loader color

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,12 +1,10 @@
-// TODO: find another loader library
-
 const Loader = () => {
   return (
     <div className="text-center">
       <div role="status">
         <svg
           aria-hidden="true"
-          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-400"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
