111e38a0dd454973aca5067d8f4369e169e71b90
Jordan <jordantseng1024@gmail.com>
2023-11-04 23:39:55 +0800

    refactor SearchVideoCard

--- app/search/SearchVideoCard.tsx
+++ app/search/SearchVideoCard.tsx
@@ -3,6 +3,7 @@ import Avatar from 'react-avatar';
 
 import { formatViews, formatTimeStamp } from '@/app/utils/helpers';
 import VideoThumbnail from '@/app/components/VideoThumbnail';
+import Link from 'next/link';
 
 type Props = {
   id: string;
@@ -38,27 +39,37 @@ const SearchVideoCard = ({
   return (
     <div className="mb-4 flex" ref={getLastVideo}>
       <div className="min-w-[150px] flex-1">
-        <VideoThumbnail thumbnail={videoThumbnail} duration={duration} id={id}/>
+        <VideoThumbnail
+          thumbnail={videoThumbnail}
+          duration={duration}
+          id={id}
+        />
       </div>
-      <div
-        className="ml-2 mt-0 flex-[3] cursor-pointer break-all"
-        onClick={handleImageClick}
-      >
-        <h4 className="line-clamp-1 overflow-hidden whitespace-normal pb-1 text-sm font-medium">
+      <div className="ml-2 mt-0 flex-[3] break-all">
+        <Link
+          className="mb-1 line-clamp-1 overflow-hidden whitespace-normal text-sm font-medium"
+          href={`/watch/${id}`}
+        >
           {title}
-        </h4>
-        <p className="line-clamp-1 overflow-hidden whitespace-normal text-xs text-light-text dark:text-dark-text">
+        </Link>
+        <Link
+          className="line-clamp-1 overflow-hidden whitespace-normal text-xs text-light-text dark:text-dark-text"
+          href={`/watch/${id}`}
+        >
           觀看次數：{formatViews(viewCount)}次・{formatTimeStamp(timeStamp)}
-        </p>
-        <div className="flex items-center gap-1 py-2">
+        </Link>
+        <Link href={`/watch/${id}`} className="flex items-center gap-1 my-2">
           <Avatar src={channelThumbnail} size="25" round />
           <p className="line-clamp-1 overflow-hidden whitespace-normal text-xs">
             {channel}
           </p>
-        </div>
-        <p className="line-clamp-1 overflow-hidden whitespace-normal text-xs text-light-text dark:text-dark-text">
+        </Link>
+        <Link
+          href={`/watch/${id}`}
+          className="line-clamp-1 overflow-hidden whitespace-normal text-xs text-light-text dark:text-dark-text"
+        >
           {description}
-        </p>
+        </Link>
       </div>
     </div>
   );
